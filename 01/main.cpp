f944b43f9b36706f42dcc7bf9a8a6713ae3c7b00
serega <ser191097@gmail.com>
2019-03-17 18:29:33 +0300

    hw1.003

--- 01/main.cpp
+++ 01/main.cpp
@@ -3,50 +3,30 @@
 #include "numbers.dat"
 
 
-int * genprime(int end, size_t &sizeprime) {
-    int size = (int) sqrt(end + 1.0);
-    int *prime = new int[size + 1];
-    int *answ = new int[size + 1];
-    for (int i = 0 ; i < size + 1; ++i) {
+int *genprime(int end, size_t &sizeprime) {
+    int *prime = new int[end + 1];
+    for (int i = 0 ; i < end + 1; ++i) {
         prime[i] = 1;
     }
     prime[0] = prime[1] = 0;
-    int k = 0;
-    int temp;
-    for(temp = 2; temp * temp <= end; ++temp) {
+    int temp = 0;
+    for(temp = 2; temp <= end; ++temp) {
         if (prime[temp]) {
-            answ[k] = temp;
-            ++k;
-            for(long long  j = temp * temp; j * j <= end; j += temp ) {
-                prime[j] = false;
+            for(long long  int j = (long long) temp * temp; j <= end; j += temp ) {
+                prime[j] = 0;
             }
         }
     }
-    sizeprime = k;
-    delete []prime;
-    return answ;
+    sizeprime = end + 1;
+    return prime;
 }
 
 
 
-bool isprime(int number, int *primes, size_t sizeprime) {
-    if (number == 1) {
-        return false;
-    }
-    int end = (int) sqrt(number + 1.0);
-    for(int i = 0; i < sizeprime; ++i) {
-        if (number == primes[i]) {
-            return true;
-        } else if (number % primes[i]  == 0) {
-            return false;
-        }
-    }
-    return true;
-}
 
 
 int startindex(const int * data, int n, int num, int start, int end) {
-    while(1) {
+    while(true) {
         int temp = (end + start) / 2;
         if (num == data[temp] && (temp == 0 || data[temp - 1] != num)) {
             return temp;
@@ -65,7 +45,7 @@ int startindex(const int * data, int n, int num, int start, int end) {
 
 
 int endindex(const int * data, int n, int num, int start, int end) {
-    while(1) {
+    while(true){
         int temp = (end + start) / 2;
         if ((num == data[temp]) && (temp == end || data[temp + 1] != num)) {
             return temp;
@@ -81,7 +61,7 @@ int endindex(const int * data, int n, int num, int start, int end) {
     }
 }
 
-
+using namespace std;
 int main(int argc, char **argv) {
     if(argc % 2 == 0 || argc == 1) {
         return -1;
@@ -92,7 +72,7 @@ int main(int argc, char **argv) {
         int begin = std::atoi(argv[i]);
         int end = std::atoi(argv[i + 1]);
         size_t sizeprime;
-        int  *primes = genprime(end, sizeprime);
+        int *primes = genprime(end, sizeprime);
         int ind1 = startindex(Data, Size, begin, 0, Size);
         int ind2 = endindex(Data, Size, end, 0, Size);
         if ((ind1 == -1) || (ind2 == -1)) {
@@ -101,7 +81,7 @@ int main(int argc, char **argv) {
         }
         counter = 0;
         for (int i = ind1; i <= ind2; ++i) {
-            if (isprime(Data[i], primes, sizeprime)) {
+            if (primes[Data[i]]) {
                 ++counter;
             }
         }
