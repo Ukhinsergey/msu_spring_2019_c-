35c79ccc4c0b2070ac7d18a679ec7c045f1f0768
serega <ser191097@gmail.com>
2019-03-25 13:38:38 +0300

    hw2.001

--- 02/main.cpp
+++ 02/main.cpp
@@ -11,100 +11,100 @@ struct Result{
 };
 
 
-struct Calculator{
+Result add_sub(string &expr);
+Result mul_div(string &expr);
+Result num(string &expr);
 
-    int64_t parse(string &expr) {
-        //remove spaces
-        for(auto i = expr.end() -1 ; i >= expr.begin(); --i) {
-            if(*i == ' ') {
-                expr.erase(i);
-            }
+int64_t parse(const string &expr1) {
+    //remove spaces
+    string expr = expr1;
+    for(auto i = expr.end() -1 ; i >= expr.begin(); --i) {
+        if(*i == ' ') {
+            expr.erase(i);
         }
-        Result res = add_sub(expr);
-        if (!res.expr.empty()) {
-            throw std::invalid_argument("error(not parse to the end) at: " + res.expr);
+    }
+  
+    Result res = add_sub(expr);
+    if (!res.expr.empty()) {
+        throw std::invalid_argument("error(not parse to the end) at: " + res.expr);
+    } else {
+        return res.res;
+    }
+}
+
+Result add_sub(string &expr) {
+    Result cur = mul_div(expr);
+    int64_t res = cur.res;
+    while(true) {
+        if(cur.expr.length() == 0) {
+            return cur;
+        }
+        char sign = cur.expr[0];
+        if(sign != '+' && sign != '-') {
+            return cur;
+        }
+        cur.expr.erase(0,1);
+        Result next = mul_div(cur.expr);
+        if (sign == '+') {
+            res = res + next.res;
         } else {
-            return res.res;
+            res = res - next.res;
         }
+        cur = Result(next.expr, res);
+
     }
-    Result add_sub(string &expr) {
-        Result cur = mul_div(expr);
-        int64_t res = cur.res;
-        while(true) {
-            if(cur.expr.length() == 0) {
-                return cur;
-            }
-            char sign = cur.expr[0];
-            if(sign != '+' && sign != '-') {
-                return cur;
-            }
-            cur.expr.erase(0,1);
-            Result next = mul_div(cur.expr);
-            if (sign == '+') {
-                res = res + next.res;
-            } else {
-                res = res - next.res;
-            }
-            cur = Result(next.expr, res);
+    return cur;
+}
 
+Result mul_div(string &expr) {
+    Result cur = num(expr);
+    int64_t res = cur.res;
+    while(true) {
+        if(cur.expr.length() == 0) {
+            return cur;
         }
-        return cur;
+        char sign = cur.expr[0];
+        if(sign != '*' && sign != '/') {
+            return cur;
+        }
+        cur.expr.erase(0,1);
+        Result next = num(cur.expr);
+        if (sign == '*') {
+            res = res * next.res;
+        } else {
+            if (next.res == 0) {
+                throw std::invalid_argument("error(division by zero)");
+            }
+            res = res / next.res;
+        }
+        cur = Result(next.expr, res);
+
     }
-    Result mul_div(string &expr) {
-        Result cur = num(expr);
-        int64_t res = cur.res;
-        while(true) {
-            if(cur.expr.length() == 0) {
-                return cur;
-            }
-            char sign = cur.expr[0];
-            if(sign != '*' && sign != '/') {
-                return cur;
-            }
-            cur.expr.erase(0,1);
-            Result next = num(cur.expr);
-            if (sign == '*') {
-                res = res * next.res;
-            } else {
-                if (next.res == 0) {
-                    throw std::invalid_argument("error(division by zero)");
-                }
-                res = res / next.res;
-            }
-            cur = Result(next.expr, res);
+    return cur;
+}
 
-        }
-        return cur;
+Result num(string &expr) {
+    int i = 0;
+    bool negative = false;
+    while(expr[i] == '-') {
+        negative = !negative;
+        ++i;
     }
-    Result num(string &expr) {
-        int i = 0;
-        bool negative = false;
-        while(expr[i] == '-') {
-            negative = !negative;
-            ++i;
-        }
-        expr.erase(0,i);
-        i = 0;
-        while(i < expr.length() && isdigit(expr[i])) {
-            ++i;
-        }
-        if (i == 0) {
-            throw std::invalid_argument("error(not number) at: " + expr);
-        }
-        int64_t temp = stoll(expr);
-        if(negative) {
-            temp = -temp;
-        }
-        expr.erase(0,i);
-        return Result(expr, temp);
+    expr.erase(0,i);
+    i = 0;
+    while(i < expr.length() && isdigit(expr[i])) {
+        ++i;
     }
-};
-
-
-
-
-
-
+    if (i == 0) {
+        throw std::invalid_argument("error(not number) at: " + expr);
+    }
+    int64_t temp = stoll(expr);
+    if(negative) {
+        temp = -temp;
+    }
+    expr.erase(0,i);
+    return Result(expr, temp);
+}
 
 
 int main(int argc, char **argv) {
@@ -114,7 +114,7 @@ int main(int argc, char **argv) {
     }
     string expr(argv[1]);
     try {
-        cout << Calculator().parse(expr) << endl;
+        cout << parse(expr) << endl;
     } 
     catch (const std::invalid_argument& ia) {
         cout << "error " << endl;
