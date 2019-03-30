328cfd5484d0166e369b83a70c4094732a96b9d1
serega <ser191097@gmail.com>
2019-03-31 01:32:58 +0300

    hw3

---
+++ 03/matrix.h
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <cstring>
+
+class Matrix {
+	const size_t rows;
+	const size_t columns;
+	const long int size;
+	int *data;
+
+	class Proxy{
+		int *row;
+		size_t columns;
+	public:
+		Proxy(int *r, size_t columns): row(r), columns(columns){};
+		int& operator[](size_t col) {
+			if(col >= columns) {
+				throw std::out_of_range("");
+			}
+			return  row[col];
+		}
+		const int& operator[](size_t col) const{
+			if(col >= columns) {
+				throw std::out_of_range("");
+			}
+			return row[col];
+		}
+	};
+
+public:
+	Matrix(size_t rows, size_t columns): rows(rows), columns(columns), size(rows * columns), data(NULL){
+		data = new int[size];
+		std::memset(data, 0, sizeof(int) * size);
+		
+	} 
+	
+	Matrix(const Matrix &a): rows(a.rows), columns(a.columns), size(rows * columns), data(NULL) {
+		data = new int[size];
+		memcpy(data, a.data, size * sizeof(int));
+	}
+	
+	~Matrix() {
+		delete[] data;
+	}
+
+	int getRows(){
+		return rows;
+	}
+
+	int getColumns(){
+		return columns;
+	}
+
+	Matrix operator *=(int n) {
+		for(int i = 0 ; i < rows; ++i) {
+			for(int j = 0 ; j < columns; ++j) {
+				(*this)[i][j] *= n;
+			}
+		}
+		return Matrix(*this);
+	}
+
+	bool operator==(const Matrix &a) const {
+		if (this == &a) {
+			return true;
+		}
+		for(int i = 0 ; i < size; ++i) {
+			if(data[i] != a.data[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool operator==(const Matrix &a) {
+		if (this == &a) {
+			return true;
+		}
+		if (columns != a.columns) {
+			return false;
+		}
+		if (rows != a.rows) {
+			return false;
+		}
+		for(int i = 0 ; i < size; ++i) {
+		}
+		for(int i = 0 ; i < size; ++i) {
+			if(data[i] != a.data[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool operator!=(const Matrix &a) const {
+		return !(*this == a);
+	}
+
+	bool operator!=(const Matrix &a) {
+		return !(*this == a);
+	}
+
+	Proxy operator[](int r) {
+		if (r >= rows) {
+			throw std::out_of_range("");
+		}
+		return Proxy(data + columns * r, columns);
+	}
+
+	const Proxy operator[](int r) const{
+		if (r >= rows) {
+			throw std::out_of_range("");
+		}
+		return Proxy(data + columns * r, columns);
+	}
+
+};
