0371b816693d98ebb284bd7436c1b16b7e9602d7
serega <ser191097@gmail.com>
2019-05-04 19:34:48 +0300

    fix_funk_prototype

--- 06/main.cpp
+++ 06/main.cpp
@@ -5,28 +5,28 @@
 
 using namespace std;
 
-const char input_file[] = "input.bin";
-const char output_file[] = "output.bin";
+const string input_file = "input.bin";
+const string output_file = "output.bin";
 using uint = unsigned int;
 
-int get_file_size(string filename)
+int get_file_size(const string *filename)
 {
     FILE *p_file = NULL;
-    p_file = fopen(filename.c_str(),"rb");
+    p_file = fopen((*filename).c_str(),"rb");
     fseek(p_file,0,SEEK_END);
     int size = ftell(p_file);
     fclose(p_file);
     return size;
 }
 
-void mergefile(string sort_file1, string sort_file2, string my_out_sort_file) {
-    FILE* out = fopen(my_out_sort_file.c_str(), "wb");
+void mergefile(const string *sort_file1,const string *sort_file2,const string *my_out_sort_file) {
+    FILE* out = fopen((*my_out_sort_file).c_str(), "wb");
 
     uint64_t sort1_size = get_file_size(sort_file1) / sizeof(uint64_t);
-    FILE* sort1 = fopen(sort_file1.c_str(), "rb");
+    FILE* sort1 = fopen((*sort_file1).c_str(), "rb");
     
     uint64_t sort2_size = get_file_size(sort_file2) / sizeof(uint64_t);
-    FILE* sort2 = fopen(sort_file2.c_str(), "rb");
+    FILE* sort2 = fopen((*sort_file2).c_str(), "rb");
 
     uint64_t temp1, temp2;
     fread(&temp1, sizeof(temp1), 1, sort1);
@@ -73,7 +73,7 @@ string mergesort(uint64_t left, uint64_t right, uint depth) {
     if (left == right) {
         string my_out_sort_file = string(to_string(left) + '-' + to_string(right) + '-' + to_string(depth));
         FILE* out = fopen(my_out_sort_file.c_str(), "wb");
-        FILE* in = fopen(input_file, "rb");
+        FILE* in = fopen(input_file.c_str(), "rb");
         fseek(in, sizeof(uint64_t) * left, SEEK_SET);
         uint64_t temp;
         fread(&temp, sizeof(temp), 1, in);
@@ -91,7 +91,7 @@ string mergesort(uint64_t left, uint64_t right, uint depth) {
     sort_file2 = mergesort(middle + 1, right, depth + 1);
 
     string my_out_sort_file = string(to_string(left) + '-' + to_string(right) + '-' + to_string(depth));
-    mergefile(sort_file1, sort_file2, my_out_sort_file);
+    mergefile(&sort_file1, &sort_file2, &my_out_sort_file);
 
     return my_out_sort_file;
 }
@@ -110,7 +110,7 @@ int main(int argc, char **argv)
     sscanf(argv[2], "%d", &size);
 
     if(random == 1) {
-        FILE* in = fopen(input_file, "wb"); 
+        FILE* in = fopen(input_file.c_str(), "wb"); 
         srand(time(NULL)); 
         // generate input 
         for (int i = 0; i < size; i++) {
@@ -119,7 +119,7 @@ int main(int argc, char **argv)
         } 
         fclose(in); 
     }
-    uint64_t nums = get_file_size(input_file) / sizeof(uint64_t);
+    uint64_t nums = get_file_size(&input_file) / sizeof(uint64_t);
 
     thread t1(mergesort, 0, (nums - 1) / 2, 0);
     thread t2(mergesort, (nums - 1) / 2 + 1, nums - 1, 0);
@@ -127,6 +127,6 @@ int main(int argc, char **argv)
     t2.join();
     string str1 = string(to_string(0) + '-' + to_string((nums - 1) / 2) + '-' + to_string(0));
     string str2 = string(to_string((nums - 1) / 2 + 1) + '-' + to_string(nums - 1) + '-' + to_string(0));
-    mergefile(str1, str2, "output.bin");
+    mergefile(&str1, &str2, &output_file);
     return 0;
 }
